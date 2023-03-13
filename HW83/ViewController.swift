025398c9b898e9c4798a0af0c5b854f4f0aa90b5
Datkayim <dkeldibekova08@gmail.com>
2023-03-13 11:02:08 +0600

    Initial Commit

---
+++ HW83/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW83
+//
+//  Created by Jarae on 13/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
